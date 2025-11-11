877552f327a3c3580b559b689e776c86c3c78f75
Serhii Kalenichenko <serhiy.kalenichenko@gmail.com>
2025-11-11 22:41:39 +0200

    refactor: remove unused files and implement initial structure for address book and CLI functionality

--- conftest.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SRC = os.path.join(BASE_DIR, "src")
-ASSISTANT = os.path.join(SRC, "assistant")
-for p in (SRC, ASSISTANT):
-    if p not in sys.path:
-        sys.path.insert(0, p)
